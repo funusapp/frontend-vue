eba9f517da39d354d7d6ecea6db1b63ec1bba296
Funus App <funusapp@gmail.com>
2024-07-08 21:25:32 -0300

    Cap. 3 - Entendendo o funcionamento do Vue

--- helloworld/src/main.js
+++ helloworld/src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+createApp({
+  render () {
+    return (
+      <header>
+        <h1>Titulo do post</h1>
+        <p>Conteudo da postagem</p>
+      </header>
+    );
+  }
+}).mount('#app');
